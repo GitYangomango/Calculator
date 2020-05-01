9e0f2576551bf503318adbd1d6ff321694035a7f
GitYangomango <64616776+GitYangomango@users.noreply.github.com>
2020-05-01 09:59:18 +0200

    Add files via upload

---
+++ xiang.yang.4G.Calcolatrice/xiang.yang.4G.Calcolatrice/Models/Operando.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operandocs
+{
+    public class Operando
+    {
+        private String valore;
+        private Operando next;
+
+        public Operando(String valore)
+        {
+            this.valore = valore;
+            next = null;
+        }
+
+        public String GetValore()
+        {
+            return valore;
+        }
+        public Operando GetNext()
+        {
+            return next;
+        }
+        public void SetNext(Operando p)
+        {
+            next = p;
+        }
+    }
+}
---
+++ xiang.yang.4G.Calcolatrice/xiang.yang.4G.Calcolatrice/Models/Pila.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Operandocs;
+
+namespace Pilacs
+{
+    class Pila
+    {
+        private Operando first;
+        private int nnodi;
+
+        public Pila()
+        {
+            nnodi = 0;
+        }
+
+        public void Push(String valore)
+        {
+            Operando a = new Operando(valore);
+
+            if (nnodi == 0)
+            {
+                first = a;
+                nnodi++;
+            }
+            else
+            {
+                a.SetNext(first);
+                first = a;
+                nnodi++;
+            }
+        }
+        public String Pop()
+        {
+            Operando p;
+            if (nnodi == 0)
+                return "Lista vuota";
+            p = first;
+            first = p.GetNext();
+            nnodi--;
+            return p.GetValore();
+        }
+        public String toString()
+        {
+            String s = "";
+            Operando p;
+            p = first;
+            if (p == null)
+                s = "Non ci sono numeri";
+            else
+            {
+                while (p != null)
+                {
+                    s = s + p.GetValore();
+                    p = p.GetNext();
+                }
+            }
+            return s;
+        }
+    }
+}
